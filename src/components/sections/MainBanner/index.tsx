0ec42887b103d7008ec70f3dce22c44ff9b289b7
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-08-08 23:42:36 -0300

    fixs on first section

--- src/components/sections/MainBanner/index.tsx
+++ src/components/sections/MainBanner/index.tsx
@@ -4,14 +4,17 @@ import { Section } from '@/components/Section'
 import './style.css'
 export const MainBanner = () => {
   return (
-    <Section className='pb-32 pt-16 firstSection'>
-      <img src={logo} alt='logo' className='max-w-[312px]'/>
-      <div className="flex items-center gap-6">
-        <div>
-          <h1 className='font-extrabold text-4xl'>28 Dias para Mudar</h1>
-          <p className='font-bold'>A Revolução do seu Corpo, Mente e Estilo de Vida!</p>
-        </div>
-        <ArrowButton/>
+    <Section className='relative h-[512px] flex flex-col items-center justify-center'>
+      <div className='absolute w-screen h-full left-0 top-0 firstSection grayscale z-0 '></div>
+      <div className='relative z-20'>
+        <img src={logo} alt='logo' className='max-w-[512px]'/>
+        <div className="flex items-center gap-12">
+          <div>
+            <h1 className='font-extrabold text-5xl mb-2'>28 Dias para Mudar</h1>
+            <p className='font-bold text-2xl'>A Revolução do seu Corpo, Mente e Estilo de Vida!</p>
+          </div>
+          <ArrowButton size={64}/>
+      </div>
       </div>
     </Section>
   )
