d4c418f1954a19c88755f5a993e4f0b2f406bcb6
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-08-08 22:27:54 -0300

    created tab effect on desktop

--- src/components/ImageWithBorder/index.tsx
+++ src/components/ImageWithBorder/index.tsx
@@ -9,23 +9,22 @@ type ImageWithBorderProps = {
 
 export const ImageWithBorder = ({ image, altText, width, height, hasTabEffect = true, inverse = false }: ImageWithBorderProps) => {
   return (
-    <div className={`relative rounded-lg min-h-[128px] ${inverse ? 'flex-row-reverse' : ''}`}>
+    <div className="relative rounded-lg min-h-[128px]">
       <div 
-        className={`w-full z-20 absolute border border-orange-seu-treino rounded-xl mt-2 ${inverse ? 'right-0' : 'left-0'}`} 
-        style={{ height: height, width: width, transform: inverse ? 'scaleX(-1)' : 'none' }}
+        className="w-full z-20 absolute border border-orange-seu-treino rounded-xl mt-2" 
+        style={{ height: height, width: width }}
       ></div>
       
       {hasTabEffect && (
         <div 
-          className={`bg-orange-seu-treino absolute w-[50px] h-[50px] rounded-xl top-[40%] z-0`} 
-          style={{ left: inverse ? 'auto' : '-3%', right: inverse ? '-3%' : 'auto', transform: inverse ? 'scaleX(-1)' : 'none' }}
+          className={`bg-orange-seu-treino absolute w-[50px] h-[50px] rounded-xl top-[40%] z-0 ${inverse? 'right-[3%]':'left-[-3%]'}` }
         ></div>
       )}
       
       <img 
         src={image} 
         alt={altText} 
-        className={`z-10 relative -ml-2 rounded-xl h-full w-full object-cover`}  
+        className={`z-10 relative rounded-xl h-full w-full object-cover ${inverse ? 'ml-2' : '-ml-2'}`}  
         style={{ height: height, width: width }}
       />
     </div>
