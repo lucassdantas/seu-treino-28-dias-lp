e51dd882ac324b1d4f56746f6df5a6d1a8f2d9eb
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-08-28 22:35:23 -0300

    change arrow button to normal buttons

--- src/components/sections/MainBanner/index.tsx
+++ src/components/sections/MainBanner/index.tsx
@@ -1,5 +1,5 @@
 import logo from '@/assets/logo-seu-treino-linha-branca-breno-silva.png'
-import { ArrowButton } from '@/components/Button'
+import { ArrowButton, Button } from '@/components/Button'
 import { Section } from '@/components/Section'
 import './style.css'
 export const MainBanner = () => {
@@ -13,7 +13,7 @@ export const MainBanner = () => {
             <h1 className='font-extrabold text-5xl mb-2'>28 Dias para Mudar</h1>
             <p className='font-bold text-2xl'>A Revolução do seu Corpo, Mente e Estilo de Vida!</p>
           </div>
-          <ArrowButton size={64}/>
+          <Button/>
         </div>
       </div>
     </Section>
--- src/components/sections/TransformYourLife/index.tsx
+++ src/components/sections/TransformYourLife/index.tsx
@@ -1,4 +1,4 @@
-import { ArrowButton } from '@/components/Button'
+import { Button } from '@/components/Button'
 import { Section } from '@/components/Section'
 
 export const TransformYourLife = () => {
@@ -15,11 +15,10 @@ export const TransformYourLife = () => {
             transformação completa do corpo e da mente. </p>
         </div>
         <div className='flex flex-col items-center gap-4'>
-          <ArrowButton />
+          <Button />
           <h2 className='text-4xl font-bold text-orange-seu-treino mt-6'>Seu Treino - 28 dias para mudar</h2>
           <p>Transforme seu corpo. Transforme sua mente. Transforme sua vida.</p>
           <div className='w-1/2 bg-orange-seu-treino h-[2px] rounded-xl opacity-25 mt-12'></div>
-          {/*line*/}
         </div>
       </div>
     </Section>
