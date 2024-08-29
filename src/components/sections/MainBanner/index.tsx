8338d2af401255ca76d1139924b3cbb1022cf2b6
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-08-28 22:44:31 -0300

    fix button size by div calss

--- src/components/sections/MainBanner/index.tsx
+++ src/components/sections/MainBanner/index.tsx
@@ -9,7 +9,7 @@ export const MainBanner = () => {
       <div className='relative z-20 p-2'>
         <img src={logo} alt='logo' className='max-w-[512px] w-full'/>
         <div className="flex lg:flex-row flex-col items-start lg:items-center md:gap-12 gap-4">
-          <div>
+          <div className='flex flex-col'>
             <h1 className='font-extrabold text-5xl mb-2'>28 Dias para Mudar</h1>
             <p className='font-bold text-2xl'>A Revolução do seu Corpo, Mente e Estilo de Vida!</p>
             <Button text='Quero começar agora'/>
